6d737cff1745cf34e71624a21a4d6af29547fab7
hurivono <jhyoon9966@gmail.com>
2024-10-08 21:58:49 +0900

    Update app.py
    node name extract code added

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     pod_name = os.environ.get('POD_NAME', 'Unknown Pod')
+    node_name = os.environ.get('NODE_NAME', 'Unknown Pod')
     pod_namespace = os.environ.get('POD_NAMESPACE', 'Unknown Namespace')
     
-    return f'Pod Name: {pod_name}<br>Pod Namespace: {pod_namespace}'
+    return f'Pod Name: {pod_name}<br>Node Name: {node_name}<br>Pod Namespace: {pod_namespace}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
