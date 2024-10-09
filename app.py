9becb4e7365012ec211005303d2c1f2d15af27d3
hurivono <jhyoon9966@gmail.com>
2024-10-10 00:00:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     node_name = os.environ.get('NODE_NAME', 'Unknown Pod')
     pod_namespace = os.environ.get('POD_NAMESPACE', 'Unknown Namespace')
     
-    return f'Pod Name: {pod_name}<br>Node Name: {node_name}<br>Pod Namespace: {pod_namespace}'
+    return f'Pod Name: {pod_name}<br>Node Name: {node_name}<br>Pod Namespace: {pod_namespace}, version: 241009_02'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
